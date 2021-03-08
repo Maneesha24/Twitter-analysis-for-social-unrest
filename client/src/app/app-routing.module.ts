fb0a6137e7fb5084aae5c04b8f23ca5587dc6d89
Maneesha Venigalla <maneeshavenigalla@Maneeshas-MacBook-Pro.local>
2021-03-08 10:44:09 -0500

    Implemented angular routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'statistics',
+    pathMatch: 'full',
+    loadChildren: () => import('./modules/statistics/statistics.module').then((m) => m.StatisticsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
